30ac90921cfe1996ef160a788e0f9435cc06b36a
em <michael@joinbox.com>
2016-02-01 19:01:54 +0100

    complete rewrite

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
+(function() {
+	'use strict';
 
-	var   parsed = null
-		, parse;
+	let log = require('ee-log');
 
-	parse = function(){
-		var argv;
 
-		parsed = {};
+	class PseudoSet extends Array {
 
-		argv = Array.prototype.slice.call(process.argv, 2).join(' ').split('--').filter(function(item) { 
-			return item.length > 0; 
-		}).forEach(function(item) {
-			var reg = /([^ =]+)(?: |=)(.+)/gi.exec(item.trim());
+		has(item) {
+			return this.indexOf(item) >= 0;
+		}
+	}
 
-			if (reg) parsed[reg[1].trim()] = reg[2];
-			else parsed[item.trim()] = null;
-		}.bind(this));
+
+
+	let args = new Map();
+	args.anonymous = new PseudoSet();
+
+
+
+	let parseResult = (input) => {
+		if (/[^0-9\.]/.test(input)) {
+			if (input.toLowerCase() === 'true') return true;
+			else if (input.toLowerCase() === 'false') return false;
+			else return input;
+		}
+		else if (/[^0-9]/.test(input)) return parseFloat(input);
+		else return parseInt(input, 10);
 	};
 
 
 
-	module.exports = {
-		get: function(key) {
-			if (!parsed) parse();
-			return parsed[key];
-		}
+	Array.prototype.slice.call(process.argv, 2).forEach((item) => { log(item);
+		if (/^\s*--([^=]+)=(.+)\s*$/gi.test(item)) {
+			let result = /^\s*--([^=]+)=(.+)\s*$/gi.exec(item);
 
-		, has: function(key) {
-			if (!parsed) parse();
-			return parsed.hasOwnProperty(key);
+			args.set(result[1], parseResult(result[2]));
 		}
-	};
+		else if (/^\s*-(\S+)\s(.+)\s*$/gi.test(item)) {
+			let result = /^\s*-(\S+)\s(.+)\s*$/gi.exec(item);
+
+			args.set(result[1], parseResult(result[2]));
+		}
+		else if (/^\s*--([^\s]+)\s*$/gi.test(item)) {
+			let result = /^\s*--([^\s]+)\s*$/gi.exec(item);
+
+			args.set(result[1], true);
+		}
+		else args.anonymous.push(item);
+	});
+
+ 
+	module.exports = args;
+})();
--- test/index.js
+++ test/index.js
@@ -1,17 +1,65 @@
+(function() {
+    'use strict';
 
 
-
-    var   argv      = require('../')
-        , util      = require('util')
-        , assert    = require('assert');
+    let log = require('ee-log');
 
 
-    describe('The parser', function() {
-        it('should be able to test for values from the argv string', function(){
-            assert.ok(argv.has('reporter'));
+    let assert = require('assert');
+
+    process.argv.push('--a=1');
+    process.argv.push('--abc=2.0 ');
+    process.argv.push('     --b=true');
+
+    process.argv.push('-c 1');
+    process.argv.push('-cde 2 ');
+    process.argv.push('     -d hui');
+
+    process.argv.push('d');
+
+
+    let argv = require('../');
+
+
+    describe('Parser', function() {
+        it('double - argument', function(){
+            assert.equal(argv.get('a'), 1);
         });
 
-         it('should be able to retreive values from the argv string', function(){
-            assert.equal(argv.get('reporter'), 'spec');
+        it('double - argument with trailing whitespace', function(){
+            assert.equal(argv.get('abc'), 2);
+        });
+
+        it('double - argument staring with whitespace', function(){
+            assert.equal(argv.get('b'), true);
+        });
+
+
+
+        it('single - argument', function(){
+            assert.equal(argv.get('c'), 1);
+        });
+
+        it('single - argument with trailing whitespace', function(){
+            assert.equal(argv.get('cde'), 2);
+        });
+
+        it('single - argument staring with whitespace', function(){
+            assert.equal(argv.get('d'), 'hui');
+        });
+
+
+
+        it('anonymous argument', function(){
+            assert.equal(argv.anonymous[1], 'd');
+        });
+
+
+
+        it('flags', function(){
+            assert.equal(argv.has('reporter'), true);
         });
     });     
+})();
+
+
